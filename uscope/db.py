64ce132ce3084591302c3a84a5ff3652a7f6608f
johnmatthews7234 <john.matthews72@gmail.com>
2022-10-18 10:07:25 +1100

    DB cleanup

--- uscope/db.py
+++ uscope/db.py
@@ -1,10 +1,12 @@
+from sys import modules
 from flask import current_app
 from flask.cli import with_appcontext
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
 from config import DATABASE as dbstring
+
 
 engine = create_engine(dbstring, echo=True, future=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
@@ -12,9 +14,14 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    from ..config import DATABASE as dbstring
+    import models
     Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main__":
-    init_db()
+def update_tables():
+    import models
+    classes = [cls_obj for cls_name, cls_obj in inspect.getmembers(modules['models']) if inspect.isclass(cls_obj)]
+    for my_class in classes:
+        if not engine.dialect.has_table(engine, my_class):
+            table = Table(my_class)
+
 
